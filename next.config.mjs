785532cf4bc58421adaa91d8d7c7eb40004e1b16
SAUMILI HALDAR <138258411+SaumiliHaldar@users.noreply.github.com>
2026-05-21 11:00:18 +0530

    Add security headers and redirects to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          }
+        ]
+      }
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.saumilihaldar.in' }],
+        destination: 'https://saumilihaldar.in/:path*',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
